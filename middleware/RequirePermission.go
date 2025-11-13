c5e9eaed1babe6d72b9bc55853c3be70208d0b8b
UserId56 <zaregay_menay@mail.ru>
2025-11-13 11:27:14 +0700

    Добавил проверку прав у роли пользователя

---
+++ middleware/RequirePermission.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"errors"
+	"httpServer/models"
+	"slices"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RequirePermission(db *gorm.DB, permission []string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userRoleId, exist := c.Get("role_id")
+		if !exist {
+			c.JSON(403, gin.H{"error": "Роль не указана"})
+			c.Abort()
+			return
+		}
+		roleId := int(userRoleId.(float64))
+		if roleId == 1 {
+			c.Next()
+			return
+		}
+		var role models.Role
+		if err := db.Model(&models.Role{}).Where("id = ?", roleId).First(&role).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(403, gin.H{"error": "Роль не найдена"})
+			c.Abort()
+			return
+		}
+		valid := true
+		for _, perm := range permission {
+			valid = valid && slices.Contains(role.Permission, perm)
+		}
+		if !valid {
+			c.JSON(403, gin.H{"error": "Недостаточно прав для выполнения этого действия"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+
+}
