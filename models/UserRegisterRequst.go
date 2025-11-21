89db05af0689f0068765bc1296a34ebf883c4ba3
UserId56 <zaregay_menay@mail.ru>
2025-11-21 10:44:05 +0700

    fix(user):Приведена общая логика для поля username

--- models/UserRegisterRequst.go
+++ models/UserRegisterRequst.go
@@ -1,7 +1,7 @@
 package models
 
 type RegisterUserRequest struct {
-	Username string `json:"user_name" binding:"required,min=3,max=50"`
+	Username string `json:"username" binding:"required,min=3,max=50"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8"`
 	Avatar   string `json:"avatar,omitempty"`
